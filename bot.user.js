daacb3491ecd63260d9ba3dd6f63819ff3957517
BoomberMan <lucascolumboo@gmail.com>
2015-08-16 13:08:12 +0200

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name        AposBot
-// @namespace   AposBot
-// @include     http://agar.io/*
+// @name        [CROatia] GOGY
+// @namespace   [CROatia] GOGY
+// @include     http://agar-hr.ga*
 // @version     3.562
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
